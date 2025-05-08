8296f2a7ebe0725052e654ba38f12a998413d7d3
Ashraf-Ali-M <ashrafjaleel786@gmail.com>
2025-05-08 13:02:50 +0500

    test

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -15,7 +15,7 @@ const Testimonial = () => {
                       {/* Testimonial 1 */}
                       <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
                           <div className="h-full text-center">
-                              <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://ecommerce-sk.vercel.app/img/kamal.png" />
+                              <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="images/arjun.jpg" />
                               <p className="leading-relaxed">CresCart has made it incredibly easy for students like me to buy, sell, and exchange products within our campus. Whether it’s a new startup product or something I no longer need, the platform offers a convenient and sustainable way to connect with others. I’ve found great deals, and I love how it promotes reusability and innovation among students.</p>
                               <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
                               <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">Arjun</h2>
@@ -26,7 +26,7 @@ const Testimonial = () => {
                       {/* Testimonial 2 */}
                       <div className="lg:w-1/3 lg:mb-0 mb-6 p-4">
                           <div className="h-full text-center">
-                              <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://www.devknus.com/img/gawri.png" />
+                              <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="images/peter.jpg" />
                               <p className="leading-relaxed">As an IoT student, I am always on the lookout for innovative solutions, and CresCart has been a great platform to explore products and ideas that align with my interests. I’ve been able to both sell some of my unused gadgets and buy useful tech items from other students. It's a convenient and eco-friendly way to support the campus community.</p>
                               <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
                               <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">Peter Prabhu</h2>
@@ -37,7 +37,7 @@ const Testimonial = () => {
                       {/* Testimonial 3 */}
                       <div className="lg:w-1/3 lg:mb-0 p-4">
                           <div className="h-full text-center">
-                              <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="https://firebasestorage.googleapis.com/v0/b/devknus-official-database.appspot.com/o/images%2FScreenshot%202023-07-07%20at%202.20.32%20PM-modified.png?alt=media&token=324ddd80-2b40-422c-9f1c-1c1fa34943fa" />
+                              <img alt="testimonial" className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-gray-200 bg-gray-100" src="images/thalha.jpg" />
                               <p className="leading-relaxed">CresCart is a wonderful platform for students like me to discover and share products within the campus. As an Aids student, I appreciate how easy it is to find useful items and sell things that I no longer need. The platform promotes sustainability, and it feels great to be part of this eco-conscious initiative.</p>
                               <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
                               <h2 className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase">Thalha</h2>
