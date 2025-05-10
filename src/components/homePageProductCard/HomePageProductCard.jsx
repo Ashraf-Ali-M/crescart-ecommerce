aef64f09d210d6f287eb9a68346e42a226a14e0c
Ashraf-Ali-M <152702100+Ashraf-Ali-M@users.noreply.github.com>
2025-05-10 12:06:21 +0500

    Update HomePageProductCard.jsx

--- src/components/homePageProductCard/HomePageProductCard.jsx
+++ src/components/homePageProductCard/HomePageProductCard.jsx
@@ -68,7 +68,7 @@ const HomePageProductCard = () => {
                         CresCart
                       </h2>
                       <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
-                        {title.substring(0, 25)}
+                        {title.substring(0, 32)}
                       </h1>
                       <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
                         ₹{price}
