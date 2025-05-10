a26917ee7fa67fe2e6a579bca809a63e4176d6fb
Ashraf-Ali-M <152702100+Ashraf-Ali-M@users.noreply.github.com>
2025-05-10 14:23:28 +0500

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -66,7 +66,7 @@ const Footer = () => {
                                 viewBox="0 0 24 24"
                             >
                                 <rect width={20} height={20} x={2} y={2} rx={5} ry={5} />
-                                <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01" />
+                                <path d="https://www.instagram.com/crescart_official?igsh=MWlzY3BnZ24ycTBmbw==" />
                             </svg>
                         </a>
 
